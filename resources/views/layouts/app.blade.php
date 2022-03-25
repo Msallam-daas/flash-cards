abe984dea06378ded97dc64fb65efeb79390f429
Msallam-daas <msallam.daas@gmail.com>
2022-03-25 20:28:26 +0300

    finshed home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,20 +24,23 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{url('css/card.css')}}" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.15.4/css/all.css">
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white ">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="{{asset('images/flash_cards.png')}}" alt="logo" />
+                <img src="{{asset('images/flash_cards.png')}}"  alt="logo" />
                 </a>
                
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -72,9 +75,7 @@
                                 <li class="nav-item ">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Reviews') }}</a>
                                 </li>
-                                <li class="nav-item ">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Study plan') }}</a>
-                                </li>
+                              
 
                                 @endguest
                     </ul>
